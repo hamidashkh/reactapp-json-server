b6a2c9d68e5a1038ed3c363de38d3d8e5622fed4
hamidashkh <hamidashkh@code.edu.az>
2022-12-12 00:22:34 +0400

    added loader

--- src/App.js
+++ src/App.js
@@ -4,10 +4,23 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import AddUser from './pages/AddUser';
 import axios from 'axios'
-import { useState,useEffect } from 'react';
+import { useState,useEffect,CSSProperties } from 'react';
+
+import ClimbingBoxLoader  from "react-spinners/ClimbingBoxLoader";
+
+const override = {
+  display: "block",
+  margin: "0 auto",
+  position:"absolute",
+  top:"50%",
+  left:"50%",
+  transform:"translate(-50%,-50%)"
+};
 
 function App() {
   const [users,setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  let [color, setColor] = useState("#0095ED");
 
   useEffect(() =>{
     const getUsers= async () =>{
@@ -16,6 +29,7 @@ function App() {
       .get('http://localhost:3030/posts')
       .then((res) => setUsers(res.data))
       .catch((err) => console.log(err));
+      setLoading(false)
     }
       getUsers();
   },[])
@@ -24,10 +38,18 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Header/>
-        <Routes>
-          <Route path='/' element={<Home userList={users} />}/>
-          <Route path='/add' element={<AddUser/>}/>
-        </Routes>
+      {loading ? <ClimbingBoxLoader 
+        color={color}
+        loading={loading}
+        cssOverride={override}
+        size={30}
+        
+      /> : <Routes>
+      <Route path='/' element={<Home userList={users} />}/>
+      <Route path='/add' element={<AddUser/>}/>
+    </Routes>
+      }
+        
       </BrowserRouter>
     
     </div>
